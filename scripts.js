f72ad919b6e8e23c7d0c7d1b252b4f254c820297
pckrng <pckrng@outlook.com>
2021-01-24 05:09:21 +0000

    format update

---
+++ scripts.js
@@ -0,0 +1,14 @@
+'use strict';
+    
+ 
+
+
+    gsap.timeline().from("body", {opacity: 0, duration: 1})
+    gsap.timeline().from("h1", { x: -1000, duration: 2 })
+    .from("button", {x: -300})
+    .from("li", {y: -300})
+    
+    gsap.timeline().from(".fab, .fas", {y: 500, duration: 2})
+    .from(".innerText h2", {y: 1000})
+    .from("p", {y: 1000, duration: 1})
+    
