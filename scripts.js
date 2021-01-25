eccd9337736bca4efad38fe0978627020ebe6007
pckrng <pckrng@outlook.com>
2021-01-25 14:03:50 +0000

    coming soon page added

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@
 
     gsap.timeline().from("h1", { x: -1000, duration: 2, delay: 0.5, opacity: 0 })
     .from("button", {x: -300, delay: 0.5, opacity: 0})
-    .from("li", {y: -300})
+    .from("li", {y: -300, opacity: 0.2, duration: 1})
     
-    gsap.timeline().from(".fab, .fas", {y: 500, duration: 2})
+    gsap.timeline().from(".fab, .fas", {y: 500, duration: 2, opacity: 0})
     .from(".innerText h2", {y: 1000})
     
     .from("p", {y: 1000, duration: 1})
