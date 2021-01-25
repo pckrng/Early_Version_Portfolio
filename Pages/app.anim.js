ddefeba5b9a14cb56b957e732021074ba4dba6b3
pckrng <pckrng@outlook.com>
2021-01-25 05:22:02 +0000

    styles

--- Pages/app.anim.js
+++ Pages/app.anim.js
@@ -1,5 +1,5 @@
 var tl = gsap.timeline({repeatDelay: 1});
-    gsap.from("body", {opacity: 0, duration: 1, y: -1000, duration: 4, ease: 3});
+    gsap.from("body", {opacity: 0, duration: 1, y: -1000, duration: 2, ease: 3});
 
     function doFade() {
   tl.restart();
