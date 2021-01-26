685a30f2045a2408f6aa5e749be8671107dc9538
pckrng <pckrng@outlook.com>
2021-01-26 03:27:00 +0000

    pages added

--- Pages/portfolio.js
+++ Pages/portfolio.js
@@ -1,2 +1,32 @@
 gsap.from(".card", {opacity: 0,  x: -1000, duration: 1, ease: 3, delay: 2});
-gsap.from("nav", {opacity: 0, duration: 1, delay: 1, x: -400})
+gsap.from("nav", {opacity: 0, duration: 1, delay: 1, x: -400});
+
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+	 
+// Image List
+images[0] = "image04.jpg";
+images[1] = "image03.jpg";
+images[2] = "image02.jpg";
+images[3] = "image01.jpg";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@
 
 
     gsap.timeline().from("h1", { x: -1000, duration: 2, delay: 0.5, opacity: 0 })
-    .from("button", {x: -300, delay: 0.5, opacity: 0})
-    .from("li", {y: -300, opacity: 0.2, duration: 1})
+    .from("button", {x: -300, opacity: 0})
+    .from("li", {y: -300, opacity: 0.2, duration: 0.5})
     
     gsap.timeline().from(".fab, .fas", {y: 500, duration: 2, opacity: 0})
     .from(".innerText h2", {y: 1000})
